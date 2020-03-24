6fd84b165546aa9d251abac893063356fa2a51af
Godwin Owoicho Onuh <godwinoyale@gmail.com>
2020-03-24 18:13:53 +0100

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,5 @@
+from math import pi
+radius_circle = float(input("Please enter the radius of the circle(cm): "))
+area_circle = pi * radius_circle**2
+print("The area of a circle with radius " + str(radius_circle) + " is: " + str(area_circle))
+
